fc528289bf6b611657af2e1a50e567acfd8f30bb
macaserta <maria.caserta@cern.ch>
2026-04-16 12:59:10 +0200

    Update cuts.py

--- Full2023BPixv12/cuts.py
+++ Full2023BPixv12/cuts.py
@@ -2,86 +2,86 @@ cuts = {}
 
 _tmp = [
     #'Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
+    #preselection paper WW 2022
     'Lepton_pt[0] > 25.',
-    'Lepton_pt[1] > 10.',
-    '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1] > 13.)',
-    '(nLepton >= 2 && Alt(Lepton_pt,2, 0) < 10.)',
-    'ptll>15',
-    'mll > 12',
-    '(zeroJet || Sum(CleanJet_pt>30.0)<=3)',
-    'noJetInHorn'
+    'Lepton_pt[1] > 20.',
+   # '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1] > 13.)',
+    '(nLepton >= 2 && Alt(Lepton_pt,2, 0) < 10.)', #to suppress backgrounds from WZ and ZZ processes
+    #'ptll>15',
+    #To reduce top quark background contributions, events with one or more b-tagged jets are rejected.
+    #'mll > 85', #to reject Z → 𝜏+𝜏− and Higgs boson background events
+    #'(zeroJet || Sum(CleanJet_pt>30.0)<=3)',
+    'noJetInHorn_pT30'
 ]
 
 preselections = ' && '.join(_tmp)
 
-cuts['Zee']  = {
-   'expr' : '(Lepton_pdgId[0] * Lepton_pdgId[1] == -11*11) && mll > 60 && mll < 120',
-   'categories' : {
-       '0j' : 'zeroJet',
-       '1j' : 'oneJet && Alt(CleanJet_pt,1,0)<30',
-       '2j' : 'multiJet',
-       'Inc' : '1',
-  }
-}
 
-cuts['Zmm']  = {
-    'expr' : '(Lepton_pdgId[0] * Lepton_pdgId[1] == -13*13) && mll > 60 && mll < 120',
+cuts['SR'] = {
+    'expr': 'mll > 85 && bVeto && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
     'categories' : {
-        '0j' : 'zeroJet',
-        '1j' : 'oneJet && Alt(CleanJet_pt,1,0)<30',
-        '2j' : 'multiJet',
-        'Inc' : '1',
+        '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0 && Lepton_pt[1]>=20',
+        '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0 && Lepton_pt[1]>=20',
+        '2j' : 'Sum(CleanJet_pt>30.0)==2',
+        'maj3j' : 'Sum(CleanJet_pt>30.0)>=3',
+        'Inc': 'mll>12',
     }
 }
 
-cuts['hww2l2v_13TeV_ss'] = {
-    'expr': 'bVeto && mll>35 && mpmet<30 && Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13',
+cuts['TopCR']  = {
+   'expr' : 'mll > 85 && ( ((zeroJet && !bVeto) || bReq1)  || bReq2 ) && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
+   'categories' : {
+       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
+       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
+       '2j' : 'Sum(CleanJet_pt>30.0)==2',
+       'maj3j' : 'Sum(CleanJet_pt>30.0)>=3',
+       'Inc': 'mll>12',
+   }
+}
+cuts['TopCR_bReq2']  = {
+   'expr' : 'mll > 85 && bReq2  && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
+   'categories' : {
+       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
+       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
+       '2j' : 'Sum(CleanJet_pt>30.0)==2',
+       'maj3j' : 'Sum(CleanJet_pt>30.0)>=3',
+       'Inc': 'mll>12',
+   }
+}
+
+
+cuts['TopCR_bReq1']  = {
+   'expr' : 'mll > 85 && ((zeroJet && !bVeto) || bReq1) && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
+   'categories' : {
+       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
+       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
+       '2j' : 'Sum(CleanJet_pt>30.0)==2',
+       'maj3j' : 'Sum(CleanJet_pt>30.0)>=3',
+       'Inc': 'mll>12',
+   }
+}
+
+
+cuts['DYtautauCR']  = {
+   'expr' : 'ptll<30 && mll < 85 && bVeto && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
+   'categories' : {
+       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
+       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
+       '2j' : 'Sum(CleanJet_pt>30.0)==2',
+       'maj3j' : 'Sum(CleanJet_pt>30.0)>=3',
+       'Inc': 'mll>12',
+   }
+}
+cuts['nopromptCR'] = {
+    'expr': 'bVeto && mll>85 && Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13',
     'categories' : {
         'Inc': 'mll>12',
         '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
         '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
         '2j' : 'Sum(CleanJet_pt>30.0)==2',
+        'maj3j' : 'Sum(CleanJet_pt>30.0)>=3',
+
     }
 }
 
-cuts['hww2l2v_13TeV_sr'] = {
-    'expr': 'sr && mpmet>15 && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
-    'categories' : {
-        '0j_pt2gt20' : 'Alt(CleanJet_pt,0, 0.0)<30.0 && Lepton_pt[1]>=20',
-        '0j_pt2lt20' : 'Alt(CleanJet_pt,0, 0.0)<30.0 && Lepton_pt[1]<20',
-        '1j_pt2gt20' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0 && Lepton_pt[1]>=20',
-        '1j_pt2lt20' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0 && Lepton_pt[1]<20',
-        '2j' : 'Sum(CleanJet_pt>30.0)==2',
-        'Inc': 'mll>12',
-    }
-}
 
-cuts['hww2l2v_13TeV_ww']  = {
-   'expr' : 'wwcr && mpmet>15 && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
-   'categories' : {
-       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
-       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
-       '2j' : 'Sum(CleanJet_pt>30.0)==2',
-       'Inc': 'mll>12',
-   }
-}
-
-cuts['hww2l2v_13TeV_top']  = { 
-   'expr' : 'topcr && mpmet>15 && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
-   'categories' : {
-       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
-       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
-       '2j' : 'Sum(CleanJet_pt>30.0)==2',
-       'Inc': 'mll>12',
-   }
-}
-
-cuts['hww2l2v_13TeV_dytt']  = { 
-   'expr' : 'dycr && Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13',
-   'categories' : { 
-       '0j' : 'Alt(CleanJet_pt,0, 0.0)<30.0',
-       '1j' : 'Alt(CleanJet_pt,0, 0.0)>30.0 && Alt(CleanJet_pt,1, 0.0)<30.0',
-       '2j' : 'Sum(CleanJet_pt>30.0)==2',
-       'Inc': 'mll>12',
-   }
-}
