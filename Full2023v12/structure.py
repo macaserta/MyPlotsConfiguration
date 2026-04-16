aaf2738a8a9f79e02e20c947c85a96eeb00d616b
macaserta <maria.caserta@cern.ch>
2026-04-16 12:54:30 +0200

    Update structure.py

--- Full2023v12/structure.py
+++ Full2023v12/structure.py
@@ -2,14 +2,87 @@
 
 structure = {}
 
-# keys here must match keys in samples.py    
-structure['DY'] = {   
-    'isSignal' : 0,
-    'isData'   : 0 
-}
 
-# data
-structure['DATA']  = { 
+
+structure['DY'] = {
     'isSignal' : 0,
-    'isData'   : 1 
-}
+    'isData'   : 0
+    }
+
+structure['top'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+structure['ggWW'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+structure['Vg'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+structure['VVV'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+structure['ZZ'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+structure['WZ'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+
+structure['VgS'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+
+structure['ggH_hww'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+
+structure['qqH_hww'] = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+
+#######################
+
+structure['WW'] = {
+    'isSignal' : 1,
+    'isData'   : 0
+    }
+
+#######################
+
+
+structure['Fake']  = {
+    'isSignal' : 0,
+    'isData'   : 0
+    }
+
+structure['DATA']  = {
+    'isSignal' : 0,
+    'isData'   : 1
+    }
+
+
+for nuis in nuisances.values():
+    if 'cutspost' in nuis:
+        print(nuis)
+        nuis['cuts'] = nuis['cutspost']
+        print(nuis)
+
+
