2a2a97f588519778e5743d14de12f049c45e459f
macaserta <maria.caserta@cern.ch>
2026-04-16 12:38:16 +0200

    Update configuration.py

--- Full2022EEv12/configuration.py
+++ Full2022EEv12/configuration.py
@@ -1,7 +1,7 @@
 import sys,os
 
 #: tag used to identify the configuration folder version
-tag = "HWW_2022EE"   
+tag = "2026__WW_2022EE_def"   
 
 #: file to use as runner script, default uses mkShapesRDF.shapeAnalysis.runner, otherwise specify path to script
 runnerFile = "default"
@@ -10,7 +10,7 @@ runnerFile = "default"
 outputFile = "mkShapes__{}.root".format(tag)
 
 #: path to ouput folder
-outputFolder = "../../../../../../../../../../../../../eos/user/s/sblancof/MC/rootFiles"
+outputFolder = "/eos/user/" + os.getlogin()[0] + "/" + os.getlogin() + "/mkShapesRDF_rootfiles/" + tag + "/rootFile/"
 #outputFolder = "rootFiles/DY_CR_2024/"
 
 # path to batch folder (used for condor submission)
@@ -47,7 +47,7 @@ structureFile = "structure.py"
 nuisancesFile = "nuisances_ALL.py"
 
 # path to folder where to save plots
-plotPath = "Plots/HWW_2022/" + tag
+plotPath = "/eos/user/m/mcaserta/www/Plots_2026/Plots/WW_2022EE_March5/" + tag
 
 # this lines are executed right before the runner on the condor node
 mountEOS = [
